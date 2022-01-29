008b434308a0f49c1e1b2914657379ef96b4a73f
Kelson Vibber <kelson@pobox.com>
2022-01-28 20:45:40 -0800

    Use WordPress' HTTP API instead of curl, fix regex, remove unused code.

--- unwrap-shortlinks.php
+++ unwrap-shortlinks.php
@@ -3,7 +3,7 @@
  Plugin Name: Unwrap Shortlinks
  Plugin URI: https://codeberg.org/kvibber/unwrap-shortlinks
  Description: Follow shortened links (t.co, bit.ly, etc) and expand them so that your blog post will point directly to the destination.
- Version: 0.2.1
+ Version: 0.2.2
  Author: Kelson Vibber
  Author URI: https://kvibber.com
  License: GPLv2 or later  
@@ -13,7 +13,7 @@
 // ini_set('display_errors', '1'); ini_set('error_reporting', E_ALL);
 
 function ktv_unwrap_shortlinks($content) {
-	preg_match_all('/[^>"]?(https?:\/\/(?:t\.co|bit\.ly|j\.mp|ow\.ly|is\.gd|trib\.al|buff\.ly|tmblr\.co|wp\.me|tinyurl\.com|goo\.gl|dlvr\.it|fb\.me|qr\.ae)\/[^\s"\']+)/', $content, $matches, PREG_PATTERN_ORDER);
+	preg_match_all('/\b(https?:\/\/(?:t\.co|bit\.ly|j\.mp|ow\.ly|is\.gd|trib\.al|buff\.ly|tmblr\.co|wp\.me|tinyurl\.com|goo\.gl|dlvr\.it|fb\.me|qr\.ae)\/[^\s"\']+)/', $content, $matches, PREG_PATTERN_ORDER);
 	foreach ($matches[1] as $link) {
 		$getlink = ktv_unwrap_shortlinks_replace($link);
 		if ($getlink != "")
@@ -25,44 +25,18 @@ function ktv_unwrap_shortlinks($content) {
 
 
 function ktv_unwrap_shortlinks_replace($url) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	curl_setopt($ch, CURLOPT_HEADER, true);
-        curl_setopt($ch, CURLOPT_MAXREDIRS, 1); // Only follow one redirect
-        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+	// make a head request and don't follow redirection, just look at the response.
+	$response = wp_remote_head( $url ); //, array( 'redirection' => 0 ) );
+	$status = wp_remote_retrieve_response_code( $response );
+	$finalURL = wp_remote_retrieve_header( $response, 'location' );
 	
-	// Retrieve content and final URL.
-        $output = curl_exec($ch);
-	$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-	$finalURL = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
-
-        curl_close($ch);
-	
-	
-	if ($output === FALSE && ! ($status == '301' || $status == '302') ) {
-		// We didn't get anything, so let's return a blank.
-		return "";
-
-	} else {
+	// If it was a redirect, return the next URL.
+	if ($status == 301 || $status == 302 || $status == 307) {
 		return esc_url($finalURL);
 	}
+	// We didn't get anything, or it didn't redirect, so let's return a blank.
+	return "";
 }
-/*
-// Takes a post ID and updates the post with the short URLs unwrapped.
-ktv_unwrap_shortlinks_update($which_post) {
-	$post_content = get_post_field('post_content', $which_post);
-	$post_content = ktv_unwrap_shortlinks($post_content);
-	$my_post = array (
-		'ID' => $which_post,
-		'post_content' => $post_content
-	);
-	wp_update_post( $my_post );
-}
-*/
-
-//add_filter('the_content', 'ktv_unwrap_shortlinks');
 
 add_filter('content_save_pre', 'ktv_unwrap_shortlinks', 700);
 
